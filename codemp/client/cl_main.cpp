af83fcab08411bbb2a2488b33ce8bad0dab648bf
redsaurus <redsaur@redsaurus.net>
2015-02-24 21:42:31 +0000

    Renamed main executable and renderer.

--- codemp/client/cl_main.cpp
+++ codemp/client/cl_main.cpp
@@ -2364,7 +2364,7 @@ static IHeapAllocator *GetG2VertSpaceServer( void ) {
 	return G2VertSpaceServer;
 }
 
-#define DEFAULT_RENDER_LIBRARY "rd-vanilla"
+#define DEFAULT_RENDER_LIBRARY "rd-vanilla-mbii"
 
 void CL_InitRef( void ) {
 	static refimport_t ri;
