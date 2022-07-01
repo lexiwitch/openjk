e19ded763b5cf60ba0f57c333afc8494440f4996
Spaghetti <strike756@gmail.com>
2022-07-01 17:43:04 -0600

    Vulkan-MBII: Increase MAX_DRAWIMAGES to quadruple Vanilla. MBII can hit the limit in extreme cases. Not an issue in Vanilla because the define isn't actually used there.

--- codemp/rd-vulkan/tr_local.h
+++ codemp/rd-vulkan/tr_local.h
@@ -1045,7 +1045,7 @@ void		R_Modellist_f ( void );
 //====================================================
 
 
-#define	MAX_DRAWIMAGES			4096
+#define	MAX_DRAWIMAGES			8192
 #define	MAX_LIGHTMAPS			256
 #define	MAX_SKINS				1024
 
