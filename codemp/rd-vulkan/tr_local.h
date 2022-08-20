8cf83b7a522bb7675074b576de929d6e149b429b
Spaghetti <strike756@gmail.com>
2022-08-19 21:33:28 -0600

    Doubled MAX_RENDER_COMMANDS size in Vulkan and Rend2 to match Vanilla renderer change.

--- codemp/rd-vulkan/tr_local.h
+++ codemp/rd-vulkan/tr_local.h
@@ -2053,7 +2053,7 @@ RENDERER BACK END COMMAND QUEUE
 
 =============================================================
 */
-#define	MAX_RENDER_COMMANDS	0x40000
+#define	MAX_RENDER_COMMANDS	0x80000
 
 typedef struct renderCommandList_s {
 	byte		cmds[MAX_RENDER_COMMANDS];
