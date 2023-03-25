c7dfd36de02e86b34c5418f4298354563c83630c
SomaZ <17459161+SomaZ@users.noreply.github.com>
2023-03-25 19:59:43 +0100

    Some more work on sync handling

--- codemp/rd-rend2/tr_backend.cpp
+++ codemp/rd-rend2/tr_backend.cpp
@@ -1698,9 +1698,6 @@ static const void *RB_StretchPic ( const void *data ) {
 	shader_t *shader;
 
 	cmd = (const stretchPicCommand_t *)data;
-	if (!tr.registered) {
-		return(const void *)(cmd + 1);
-	}
 
 	// FIXME: HUGE hack
 	if (!tr.renderFbo || backEnd.framePostProcessed)
@@ -1784,9 +1781,6 @@ static const void *RB_RotatePic ( const void *data )
 	shader_t *shader;
 
 	cmd = (const rotatePicCommand_t *)data;
-	if (!tr.registered) {
-		return(const void *)(cmd + 1);
-	}
 
 	// FIXME: HUGE hack
 	if (!tr.renderFbo || backEnd.framePostProcessed)
@@ -1884,9 +1878,6 @@ static const void *RB_RotatePic2 ( const void *data )
 		ratio = ((float)(SCREEN_WIDTH * glConfig.vidHeight) / (float)(SCREEN_HEIGHT * glConfig.vidWidth));
 
 	cmd = (const rotatePicCommand_t *)data;
-	if (!tr.registered) {
-		return(const void *)(cmd + 1);
-	}
 
 	// FIXME: HUGE hack
 	if (!tr.renderFbo || backEnd.framePostProcessed)
