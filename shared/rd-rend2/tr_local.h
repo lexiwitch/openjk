a198ad172b7bf5c7c5d10cd0fe7a92d3c7c4e6ae
SomaZ <17459161+SomaZ@users.noreply.github.com>
2023-10-07 02:16:31 +0200

    [rend2] Increase max fog count
    You can now load yavin_swamp again.

--- shared/rd-rend2/tr_local.h
+++ shared/rd-rend2/tr_local.h
@@ -57,6 +57,7 @@ typedef unsigned int glIndex_t;
 #define MAX_VBOS      4096
 #define MAX_IBOS      4096
 #define MAX_G2_BONES  72
+#define MAX_GPU_FOGS  24
 
 #define MAX_CALC_PSHADOWS    64
 #define MAX_DRAWN_PSHADOWS    32 // do not increase past 32, because bit flags are used on surfaces
@@ -752,7 +753,7 @@ struct FogsBlock
 
 	int numFogs;
 	float pad0[3];
-	Fog fogs[16];
+	Fog fogs[MAX_GPU_FOGS];
 };
 
 struct EntityBlock
