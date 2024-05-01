d88dc9a19afff5b030e0cfcd1854277bfb5442a8
SomaZ <17459161+SomaZ@users.noreply.github.com>
2024-05-01 13:16:30 +0200

    [rend2] Don't setup world draws when RDF_NOWORLDMODEL

--- shared/rd-rend2/tr_main.cpp
+++ shared/rd-rend2/tr_main.cpp
@@ -2517,7 +2517,7 @@ void R_SetupViewParms(const trRefdef_t *refdef)
 	R_RotateForViewer(&tr.viewParms.world, &tr.viewParms);
 	R_SetupProjection(&tr.viewParms, tr.viewParms.zNear, tr.viewParms.zFar, qtrue);
 
-	if (tr.world)
+	if (tr.world && !(refdef->rdflags & RDF_NOWORLDMODEL))
 	{
 		R_MarkLeaves();
 
@@ -2646,7 +2646,9 @@ void R_GatherFrameViews(trRefdef_t *refdef)
 {
 	int mainFlags = 0;
 	// skyportal view
-	if (tr.world && tr.world->skyboxportal)
+	if (tr.world &&
+		tr.world->skyboxportal &&
+		!(refdef->rdflags & RDF_NOWORLDMODEL))
 	{
 		tr.viewCount++;
 		tr.viewParms = tr.skyPortalParms;
@@ -2671,7 +2673,7 @@ void R_GatherFrameViews(trRefdef_t *refdef)
 	}
 
 	VectorCopy(refdef->vieworg, tr.viewParms.pvsOrigin);
-	if (tr.world)
+	if (tr.world && !(refdef->rdflags & RDF_NOWORLDMODEL))
 		R_MarkLeaves();
 
 	if (!(refdef->rdflags & RDF_NOWORLDMODEL))
